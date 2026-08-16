ccb0d9cfc9dde9afffddf5c3447ae95d9be544b2
Your Name <you@example.com>
2026-08-16 20:57:35 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -43,6 +43,11 @@ Al finalizar retorna la lista creada.
 
 List* crea_lista() {
    List* L = create_list();
+   for(int i = 1; i <= 10; i++){
+      int *dato = (int*)malloc(sizeof(int));
+      *dato = i;
+      pushback(L, dato);
+   }
    return L;
 }
 
@@ -52,7 +57,13 @@ Crea una función que reciba una lista de enteros (int*) y
 retorne la suma de sus elementos.
 */
 int sumaLista(List *L) {
-   return 0;
+   int suma = 0;
+    int *dato = (int*) first(L);
+    while (dato != NULL) {
+        suma += *dato;
+        dato = (int*) next(L);
+    }
+   return suma;
 }
 
 /*
@@ -64,8 +75,14 @@ Asume que popCurrent luego de eliminar un elemento se
 posiciona en el elemento anterior.
 */
 
-void eliminaElementos(List*L, int elem){
-
+void eliminaElementos(List *L, int elem) {
+    int *dato = (int*) first(L);
+    while (dato != NULL) {
+        if (*dato == elem) {
+            free(popCurrent(L));
+        }
+        dato = (int*) next(L);
+    }
 }
 
 /*
@@ -76,6 +93,15 @@ Puedes usar una pila auxiliar.
 */
 
 void copia_pila(Stack* P1, Stack* P2) {
+    Stack* aux = create_stack();
+    void *dato;
+    while ((dato = pop(P1)) != NULL) {
+        push(aux, dato);
+    }
+    while ((dato = pop(aux)) != NULL) {
+        push(P1, dato);
+        push(P2, dato);
+    }
 }
 
 /*
@@ -86,6 +112,28 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 */
 
 int parentesisBalanceados(char *cadena) {
-   return 0;
+    Stack *P = create_stack();
+    for (int i = 0; cadena[i] != '\0'; i++) {
+        char c = cadena[i];
+        if (c == '(') {
+            char *dato = (char*) malloc(sizeof(char));
+            *dato = c;
+            push(P, dato);
+        } else if (c == ')') {
+            if (top(P) == NULL) {
+                return 0;
+            }
+            free(pop(P));
+        }
+    }
+    if (top(P) != NULL) {
+        void *dato;
+        while ((dato = pop(P)) != NULL) {
+            free(dato);
+        }
+        return 0;
+    }
+
+    return 1;
 }
 
